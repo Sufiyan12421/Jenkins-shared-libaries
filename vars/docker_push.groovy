22f3af8d7870209a249fbc83441ac8febb25929f
Sufiyan12421 <157584300+Sufiyan12421@users.noreply.github.com>
2025-11-06 16:11:10 +0530

    Refactor image name handling and build process
    Refactor docker_push.groovy to improve error handling and streamline image build process.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,18 +1,16 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error "Image name is required"
-    def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'dockerHubCred'
-
-    echo "🚀 Pushing Docker image: ${imageName}:${imageTag}"
-
-    withCredentials([usernamePassword(
-        credentialsId: credentials,
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
-        sh """
-            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
-            docker push ${imageName}:${imageTag}
-        """
+    if (!config.imageName) {
+        error("Image name is required")
     }
+
+    def imageName = config.imageName
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+
+    echo "🚀 Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
