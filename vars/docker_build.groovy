5fafbcc4e8c13a454b1b5bd980993554398c9118
Sufiyan12421 <157584300+Sufiyan12421@users.noreply.github.com>
2025-11-06 15:53:45 +0530

    Refactor image name validation in docker_build.groovy
    Ensure imageName is validated before building.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,11 +1,15 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
+    def imageName = config.imageName
     def imageTag = config.imageTag ?: 'latest'
     def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+
+    if (!imageName) {
+        error("Image name is required")
+    }
+
+    echo "🚀 Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+
     sh """
         docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
     """
